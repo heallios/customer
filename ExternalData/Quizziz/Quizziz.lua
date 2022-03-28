4760854348b53d18fe1eb63e74d01be3435068ce
heallios <102498433+heallios@users.noreply.github.com>
2022-03-28 10:05:04 +0700

    Add files via upload

---
+++ ExternalData/Quizziz/Quizziz.lua
@@ -0,0 +1,98 @@
+﻿
+function GetConfig ()
+	return {
+        Bundle = "Assets/Bundles/Views/Quizziz/QuizzizView.prefab",
+		Anchor = "Front",
+		Config = ""
+    }
+end
+
+local _playerNamePath = "head/PlayerAvatar/nameBg/nameTxt"
+local _botNamePath = "head/BotAvatar/nameBg/nameTxt"
+
+local _loadingPanelPath ="PopupGroup/LoadingPanel"
+
+local _container = "QuestionContainer"
+
+local _quizzizPagePath = "Quizziz/QuizzizPage"
+
+local _nextPanel = "PopupGroup/NextPanel"
+
+local _countDownPanel = "PopupGroup/CountDownPanel"
+local _countDownObject = "PopupGroup/CountDownPanel/panel"
+
+local _endPanelPath = "PopupGroup/EndPanel"
+local _endPlayerName ="PopupGroup/EndPanel/head/PlayerAvatar/name"
+local _endBotName = "PopupGroup/EndPanel/head/BotAvatar/name"
+local _endTitlePath = "PopupGroup/EndPanel/head/Type"
+local _endPointpath = "PopupGroup/EndPanel/head/Score"
+
+local _quitBtnPath = "PopupGroup/EndPanel/head/BackBtn"
+
+function OnReady()
+	SetupButtonQuit(_quitBtnPath)
+
+	QuizitPvp.LuaCall_CreatePages()
+end
+
+function SetupButtonQuit(btnPath)
+	local btn = LuaGo.Find(btnPath)	
+	btn.RegisterButtonPressedCallback(function ()
+		QuizitPvp.LuaCall_GoBack()
+    end)
+end
+
+function LoadQuizzizPage()
+	local content = LuaGo.Find(_container);
+	CreateSubView(_quizzizPagePath, content.Transform)
+end
+
+function SetActiveLoadingPanel(isActive)
+	local obj = LuaGo.Find(_loadingPanelPath)
+	obj.SetActive(isActive)
+end
+
+function SetActiveEndPanel(title, point)
+	local endTitleTxt = LuaGo.Find(_endTitlePath)
+	endTitleTxt.SetText(title)
+
+	local endPointTxt = LuaGo.Find(_endPointpath)
+	endPointTxt.SetText(point)
+
+	local endPanel = LuaGo.Find(_endPanelPath)
+	endPanel.SetActive(true)
+end
+
+function SetActiveCountDown(isActive)
+	local obj = LuaGo.Find(_countDownPanel)
+	obj.SetActive(isActive)
+
+	if (isActive) then
+		local obj2 = LuaGo.Find(_countDownObject)
+		obj2.AnimationPlay("123GO")
+	end
+end
+
+function SetPlayerName(name)
+	local obj = LuaGo.Find(_playerNamePath)
+	obj.SetText(name)
+
+	local obj2 = LuaGo.Find(_endPlayerName)
+	obj2.SetText(name)
+end
+
+function SetBotName(name)
+	local obj = LuaGo.Find(_botNamePath)
+	obj.SetText(name)
+
+	local obj2 = LuaGo.Find(_endBotName)
+	obj2.SetText(name)
+end
+
+function SetActiveNextPanel(isActive)
+	local obj = LuaGo.Find(_nextPanel)
+	obj.SetActive(isActive)
+end
+
+function Hide()
+end
